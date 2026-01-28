57073445721560a86e83f5f3af118026967ec585
Gabriel_Gandrade <ggabriel.gandrade@gmail.com>
2026-01-27 23:01:47 -0300

    Configuração inicial

---
+++ src/main/java/com/ju_biel/api_gastos/ApiGastosApplication.java
@@ -0,0 +1,13 @@
+package com.ju_biel.api_gastos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGastosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGastosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ju_biel/api_gastos/ApiGastosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ju_biel.api_gastos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGastosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
